d632dd6c0d409150385145e9c30a5af537800cdc
Ensar Sami Curoglu <ensarcuroglu19@gmail.com>
2023-11-10 23:17:26 +0300

    Login Screen updated.

--- Travel Mentor/Login Screen.cs
+++ Travel Mentor/Login Screen.cs
@@ -20,22 +20,33 @@ namespace Travel_Mentor
 
         private void Login_Screen_Load(object sender, EventArgs e)
         {
-
+            labelemail.Visible = false;
+            emailText.Visible = false;
+            loginCheckBox.Checked = true;
+            
         }
 
 
         string user_name = "ensarcuroglu";
-
-        private void button1_Click(object sender, EventArgs e)
+        string password = "Ensar123";
+        
+        void clearText()
         {
-            if (user_name == usernameText.Text)
+            usernameText.Clear();
+            passwordText.Clear();
+            emailText.Clear();
+        }
+        
+        
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            if (user_name == usernameText.Text && password == passwordText.Text)
             {
                 
                 Form1 form1 = new Form1();
                 form1.ShowDialog();
-                this.Visible = false;
-
-
+                this.Visible = true;
+                
 
             }
             else
@@ -43,10 +54,45 @@ namespace Travel_Mentor
                 MessageBox.Show("Kullanıcı Adı hatalı. Lütfen tekrar deneyiniz.");
             }
 
-            
+        }
 
+        private void registerCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (registerCheckBox.Checked)
+            {
+                emailText.Visible = true;
+                labelemail.Visible = true;
+                btnLogin.Text = "Register";
+                loginCheckBox.Checked = false;
+                clearText();
+            }
+            else
+            {
+                emailText.Visible = false;
+                labelemail.Visible = false;
+                btnLogin.Text = "Login";
+                loginCheckBox.Checked = true;
+            }
+        }
 
+        private void loginCheckBox_CheckedChanged(object sender, EventArgs e)
+        {
+            if (loginCheckBox.Checked)
+            {
+                emailText.Visible = false;
+                labelemail.Visible = false;
+                btnLogin.Text = "Login";
+                registerCheckBox.Checked = false;
+                clearText();
 
+            }
+            else
+            {
+                emailText.Visible = true;
+                labelemail.Visible = true;
+                btnLogin.Text = "Register";
+                registerCheckBox.Checked = true;
+            }
 
         }
     }
