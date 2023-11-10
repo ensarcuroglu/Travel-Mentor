86c400061c840f96e1de20fd4d74ddb2cbdce4da
Ensar Sami Curoglu <ensarcuroglu19@gmail.com>
2023-11-10 22:03:03 +0300

    login screen added.

---
+++ Travel Mentor/Login Screen.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace Travel_Mentor
+{
+    partial class Login_Screen
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.usernameText = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // usernameText
+            // 
+            this.usernameText.Location = new System.Drawing.Point(158, 144);
+            this.usernameText.Name = "usernameText";
+            this.usernameText.Size = new System.Drawing.Size(125, 27);
+            this.usernameText.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(168, 299);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Login";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Login_Screen
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(472, 510);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.usernameText);
+            this.Name = "Login_Screen";
+            this.Text = "Login_Screen";
+            this.Load += new System.EventHandler(this.Login_Screen_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private TextBox usernameText;
+        private Button button1;
+    }
+}
---
+++ Travel Mentor/Login Screen.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.DataFormats;
+
+namespace Travel_Mentor
+{
+    public partial class Login_Screen : Form
+    {
+        public Login_Screen()
+        {
+            InitializeComponent();
+        }
+
+        private void Login_Screen_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+        string user_name = "ensarcuroglu";
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (user_name == usernameText.Text)
+            {
+                
+                Form1 form1 = new Form1();
+                form1.ShowDialog();
+                this.Visible = false;
+
+
+
+            }
+            else
+            {
+                MessageBox.Show("Kullanıcı Adı hatalı. Lütfen tekrar deneyiniz.");
+            }
+
+            
+
+
+
+
+        }
+    }
+}
